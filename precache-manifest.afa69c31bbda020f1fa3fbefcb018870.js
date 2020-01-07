24b077e621ffeac72402a4bd732349f8ee2caa51
unknown <radnyongtawco123@gmail.com>
2020-01-08 07:38:06 +0800

    Updates

---
+++ precache-manifest.afa69c31bbda020f1fa3fbefcb018870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742da31c7caf588eb45f009dbf256eb9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d605ba98a7374c152b68",
+    "url": "/monsters-rolodex/static/css/main.2bb72ab8.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d605ba98a7374c152b68",
+    "url": "/monsters-rolodex/static/js/main.55ccebb1.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
